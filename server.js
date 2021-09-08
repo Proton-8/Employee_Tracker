d10fab4d8b01ffd6e65f2139aa0a7b88b3261f86
Ronald Shutter <r.a.shutter.com>
2021-09-08 14:39:14 -0700

    added more details to .js code

--- server.js
+++ server.js
@@ -103,6 +103,94 @@ function tracker() {
     });
 }
 
+// -- Insert/add version by inserting row into produce table --
+// INSERT INTO produce (id, name)
+//   VALUES (1, "apple");
+
+// ---update version
+// UPDATE fiction
+// SET name = "Candide"
+// WHERE id = 2;
+
+
+// delete version
+// DELETE FROM produce
+// WHERE id = 2;
+
+
+// to review 1  ---------------
+
+// function viewAllDepartments() {
+//   db.query("SELECT department.name AS Department, department.id AS 'Department #' FROM department;", 
+//   function(err, res) {
+//     if (err) throw err
+//     console.table(res)
+//     runPrompt()
+//   })
+// }
+
+
+
+
+//   to review 2 -----------------
+
+// function addAnEmployee() {
+//   inquirer.prompt([
+//       {
+//           name: 'firstname',
+//           type: 'list',
+//           message: "What is the employee's first name?"
+//       },
+//       {
+//           name: 'lastname',
+//           type: 'list',
+//           message: "What is the employee's last name?"
+//       },
+//       {
+//           name: 'role',
+//           type: 'list',
+//           message: "What is the employee's position?",
+//           choices: selectRole()
+//       },
+//       {
+//           name: 'manager',
+//           type: 'list',
+//           message: "Who is the employee's manager?",
+//           choices: selectManager()
+//       }
+//   ]).then((val) => {
+//       var roleid = selectRole().indexOf(val.role) + 1
+//       var managerid = selectManager().indexOf(val.manager) + 1
+//       console.log(val.role)
+//       console.log(val.manager)
+//       db.query(
+//           "INSERT INTO employee SET ?",
+//           {
+//               first_name: val.firstname,
+//               last_name: val.lastname,
+//               manager_id: managerid,
+//               roles_id: roleid
+//           },(err) => {
+//               if (err) throw err
+//               console.table(val)
+//               runPrompt()
+//           }
+//       )
+//   })
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
 tracker();
 
 // do we need this???
@@ -120,3 +208,6 @@ db.query("SELECT * FROM students", function (err, results) {
 // app.listen(PORT, () => {
 //   console.log(`Server running on port ${PORT}`);
 // });
+
+
+
