3505f6ebed559b1de773a8abf449a65633281525
Ronald Shutter <r.a.shutter.com>
2021-09-10 16:24:09 -0700

    new and improved-1

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const mysql = require("mysql2");
 const PORT = process.env.PORT || 3001;
 
 // Import and require console table for data layout
-require("console.table");
+const cTable = require('console.table');
 
 // FIGfont spec in JavaScript
 const figlet = require("figlet");
@@ -20,7 +20,7 @@ const db = mysql.createConnection(
     host: "localhost",
     // MySQL username,
     user: "root",
-    //  MySQL password here
+    //  MySQL password here using 'dotenv'
     password: process.env.DB_PASSWORD,
     database: "employee_tracker_db",
   },
@@ -29,44 +29,37 @@ const db = mysql.createConnection(
 
 console.log(" ");
 
-//  EXTRA FLUFF --  Later
-
-// figlet(
-//   "EMPLOYEE         MANAGER",
-
-//   {
-//     font: "Standard",
-//     horizontalLayout: "3/4",
-//     verticalLayout: "Full",
-//   },
-
-//   function (err, data) {
-//     if (err) {
-//       console.log("Error found in Figlet...");
-//       console.dir(err);
-//       return;
-//     }
-//     console.log(data);
-//   }
-// );
-
-// setInterval(1000);
-
-// Query database
-// db.query('SELECT * FROM department', function (err, results) {
-  // console.log(results);
-//   console.table(results);
-// });
+//  EXTRA FLUFF --
+figlet.text(
+  "EMPLOYEE    TRACKER",
+  {
+    font: "Standard",
+    horizontalLayout: "default",
+    verticalLayout: "default",
+    width: 100,
+    whitespaceBreak: true,
+  },
+  function (err, data) {
+    if (err) {
+      console.log("Error found in Figlet...");
+      console.dir(err);
+      return;
+    }
+    console.log(data);
+  }
+);
 
 
+// main program ----------------
 
-function tracker() {
+const tracker = () => {
   inquirer
-  // present choices for database entry
-    .prompt([{
+    // present choices for database entry
+    .prompt([
+      {
         type: "list",
         message: "Please enter your selection....",
-        name: "choices",
+        name: "select",
         choices: [
           "View All Departments",
           "View All Roles",
@@ -75,120 +68,117 @@ function tracker() {
           "Add A Role",
           "Add An Employee",
           "Update An Employee Role",
-          "Quit"
+          "Quit",
         ],
       },
     ])
+
     // To review  -----------
     .then((entry) => {
-      getAnswerTo(entry.choices);
+      getAnswerTo(entry.select);
+      // console.log(entry.select);
     });
-  const getAnswerTo = async(reply) => {
-    switch (reply) {
-      case "View All Departments":
-        // console.log('hello world');
-        await  ViewAllDepartments();
-        // call 'view all dept' function
-        // employeeTrackerDatabase.ViewAllDepartments()
-        //   .then((results) => console.table(results))
-        //   .catch((err) => console.error(err));
-        break;
-      case "View All Roles":
-        // call 'view all roles' function
-        // employeeTrackerDatabase.GetAllRoles()
-        //   .then((results) => console.table(results))
-        //   .catch((err) => console.error(err));
-        break;
-      case "View All Employees":
-        // call 'view all employees' function
-        // employeeTrackerDatabase.GetAllEmployees()
-        //   .then((results) => console.table(results))
-        //   .catch((err) => console.error(err));
-        break;
-      case "Add Department.":
-        // call 'add dept' function
-        addDepartment();
-        break;
+};
 
-      case "Add A Role.":
-        // call 'add a role' function
-        addRole();
-        break;
+// reply = entry.select
+const getAnswerTo = async (reply) => {
+  switch (reply) {
+    case "View All Departments":
+     
+      await ViewAllDepartments();
+      // call 'view all dept' function
+      // employeeTrackerDatabase.ViewAllDepartments()
+      //   .then((results) => console.table(results))
+      //   .catch((err) => console.error(err));
+      break;
+    case "View All Roles":
+      // call 'view all roles' function
+      // employeeTrackerDatabase.GetAllRoles()
+      //   .then((results) => console.table(results))
+      //   .catch((err) => console.error(err));
+      break;
+    case "View All Employees":
+      // call 'view all employees' function
+      // employeeTrackerDatabase.GetAllEmployees()
+      //   .then((results) => console.table(results))
+      //   .catch((err) => console.error(err));
+      break;
+    case "Add Department":
+      // call 'add dept' function
+      await addDepartment();
+      break;
 
-      case "Add An Employee.":
-        //  call 'add an employee' function
-        addEmployee();
-        break;
+    case "Add A Role":
+      // call 'add a role' function
+      addRole();
+      break;
 
-      case "Update An Employee Role.":
-        // call 'update an employee role' function
-        updateEmployeeRole();
-        break;
+    case "Add An Employee":
+      //  call 'add an employee' function
+      addEmployee();
+      break;
 
-      case "Quit":
-        quit();
-        break;
-    }
-  };
+    case "Update An Employee Role.":
+      // call 'update an employee role' function
+      updateEmployeeRole();
+      break;
 
+    case "Quit":
+      quit();
+      break;
+  }
+};
 
+// //    add each function  =============
 
+// // }
 
-//    add each function  =============
-
-}
-
-async function ViewAllDepartments() {
-// Read all Dept and display
-  // const sql = `SELECT department.id AS Dept ID, 
-  // department.name AS Dept FROM department;`;
-  db.query('SELECT * FROM department', function (err, results) {
+const ViewAllDepartments = async () => {
+  // Read all Dept and display
+  db.query("SELECT * FROM department", function (err, results) {
     // console.log(results);
     console.log(" ");
-  console.table(results);
-   });
+    console.table(results);
+  });
+};
 
+const addDepartment = async () => {
+  await inquirer
+    // present choices for database entry
+    .prompt([
+      {
+        type: "input",
+        message: "Please enter the dept name",
+        name: "departmentName",
+      },
+    ])
 
+    // To review  -----------
+    .then((entry) => {
+      db.query(
+        "INSERT INTO department (name) VALUES (?)",
+        [entry.departmentName],
+        function (err, results) {
+          // console.log(results);
+          console.log(" ");
+          console.table(`Department ${entry.departmentName} added sucessfully`);
+        }
+      );
+    });
+};
 
-  // db.query(sql, (err, params, err, results) => {
-  //   if (err) {
-  //    console.table(results);
-  //   }else{
-  //     console.error(err);
-  //   }
-  //    return;
-  //      tracker();
-      
-  // });
-  
+// function ViewRoles() {}
+// function ViewEmployees() {}
+// function addDepartment() {}
 
-
-
-}
-
-function ViewRoles() {
-
-}
-function ViewEmployees() {
-
-}
-function addDepartment() {
-
-}
-
-function addRole() {
-
-}
-function addEmployee() {
-
-}
-function updateEmployeeRole() {
-
-}
+// function addRole() {}
+// function addEmployee() {}
+// function updateEmployeeRole() {}
 
 function quit() {
-  console.log('BYE')
+  console.log(" ");
+  console.log(" OK, BYE");
+  console.log(" ");
   process.quit();
 }
-
-tracker();
+setTimeout(tracker, 1000);
