20cd375e5028effcbbdc3e5de173f7e33c801576
Proton-8 <r.a.shutter@gmail.com>
2021-11-01 22:34:52 -0700

    update to server functions -6

--- server.js
+++ server.js
@@ -5,7 +5,7 @@
 const {
   default: chalk
 } = require("chalk");
-const figlet = require("figlet"); 
+const figlet = require("figlet");
 
 // Import and require inquirer
 const inquirer = require("inquirer");
@@ -22,13 +22,13 @@ const cTable = require('console.table');
 
 // Connect to database
 const db = mysql.createConnection({
-    host: "localhost",
-    // MySQL username,
-    user: "root",
-    //  MySQL password here using 'dotenv'
-    password: process.env.DB_PASSWORD,
-    database: "employee_tracker_db",
-  },
+  host: "localhost",
+  // MySQL username,
+  user: "root",
+  //  MySQL password here using 'dotenv'
+  password: process.env.DB_PASSWORD,
+  database: "employee_tracker_db",
+},
   console.log("Connected to the employee_tracker_db database.")
 
 );
@@ -65,19 +65,19 @@ const tracker = () => {
     .then((entry) => {
       getAnswerTo(entry.selection);
     });
-  const getAnswerTo =(reply) => {
+  const getAnswerTo = (reply) => {
     switch (reply) {
       case "View All Departments":
-      // View All Departments function  
-            ViewAllDepartments();
-          break;
+        // View All Departments function  
+        ViewAllDepartments();
+        break;
       case "View All Roles":
-       ViewRoles();
+        ViewRoles();
         break;
       case "View All Employees":
         // call 'view all employees' function
         ViewEmployees();
-                break;
+        break;
       case "Add Department.":
         // call 'add dept' function
         addDepartment();
@@ -106,40 +106,39 @@ const tracker = () => {
 
 
 
-  async function ViewAllDepartments() {
+  function ViewAllDepartments() {
     // Read all Dept and display
-    // const sql = `SELECT department.id AS Dept ID, 
-    // department.name AS Dept FROM department;`;
-    db.query('SELECT * FROM department', function (err, depts) {
-      console.log(depts);
-      console.log(" ");
-      console.table(depts);
-    })
-  }
+    const sql = `SELECT department.id AS Dept ID, department.name AS Dept FROM department;`;
+    db.query('SELECT * FROM department', function (err, Dept) {
+      console.log(Dept);
+    console.log(" ");
+    console.table(department);
+  })
+}
 
 
   }
 
-  function ViewRoles() {
+function ViewRoles() {
 
 
-  }
+}
 
-  function ViewEmployees() {
+function ViewEmployees() {
 
-// ???? to review
-    const sql = `SELECT employee.first_name, employee.last_name, role.title, role.salary, department.name, CONCAT(e.first_name, ' ' ,e.last_name) AS Manager FROM employee INNER JOIN role on role.id = employee.role_id INNER JOIN department on department.id = role.department_id left join employee e on employee.manager_id = e.id;`;
-    
-    db.query(sql, function(err, res) {
-        if (err) throw err
-        console.table(res)
-        startPrompt()
-    })
+  // ???? to review
+  const sql = `SELECT employee.first_name, employee.last_name, role.title, role.salary, department.name, CONCAT(e.first_name, ' ' ,e.last_name) AS Manager FROM employee INNER JOIN role on role.id = employee.role_id INNER JOIN department on department.id = role.department_id left join employee e on employee.manager_id = e.id;`;
 
-  }
+  db.query(sql, function (err, res) {
+    if (err) throw err
+    console.table(res)
+    startPrompt()
+  })
 
-  function addDepartment() {
-    inquirer
+}
+
+function addDepartment() {
+  inquirer
     .prompt([
       {
         type: "input",
@@ -161,54 +160,50 @@ const tracker = () => {
       );
     });
 
-  }
+}
 
-  function addRole() {
+function addRole() {
 
-  }
+}
 
-  function addEmployee() {
+function addEmployee() {
 
-  }
+}
 
-  function updateEmployeeRole() {
+function updateEmployeeRole() {
 
-  }
+}
 
-  function quit() {
-    console.log('BYE')
-    process.quit();
-  }
-
-
+function quit() {
+  console.log('BYE')
+  process.quit();
+}
 
 
 
-  figlet(
-    'EMPLOYEE TRACKER',
-  
-    {
-      font: "Big Money-nw",
-      horizontalLayout: "Half",
-      verticalLayout: "Half",
-      width: 100,
-      whitespaceBreak: true
-    },
-  
-    function (err, data) {
-      if (err) {
-        console.log("Error found in Figlet...");
-        console.dir(err);
-        return;
-      }
-      console.log(data);
+
+// extra / add on to the main title ----
+figlet(
+  'EMPLOYEE TRACKER',
+
+  {
+    font: "Big Money-nw",
+    horizontalLayout: "Half",
+    verticalLayout: "Half",
+    width: 100,
+    whitespaceBreak: true
+  },
+
+  function (err, data) {
+    if (err) {
+      console.log("Error found in Figlet...");
+      console.dir(err);
+      return;
     }
-  
-  )
+    console.log(data);
+  }
 
-  setTimeout(tracker, 1000);
+)
+// added timer delay to have the wording on top
+setTimeout(tracker, 1000);
 
-
-
-  // figlet();
-  // tracker();
